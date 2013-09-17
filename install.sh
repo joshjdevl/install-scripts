27f87be6329133702e00fdc309e0aa432328eff1
joshj <joshjdevl@gmail.com>
2013-09-17 13:16:53 -0500

    add python software properties for apt-add-repository

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 sudo apt-get update
-sudo apt-get -y install build-essential autoconf automake gcc g++ make finger libc6-dev pkg-config libreadline-dev zlib1g-dev libxml2-dev
+sudo apt-get -y install build-essential autoconf automake gcc g++ make finger libc6-dev pkg-config libreadline-dev zlib1g-dev libxml2-dev \
+	python-software-properties
 
