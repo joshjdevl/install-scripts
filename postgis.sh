13ceb714748b9376ade5e4a75ba6026d5f97ce68
joshj <joshjdevl@gmail.com>
2013-09-21 17:15:09 -0500

    postgis

--- postgis.sh
+++ postgis.sh
@@ -1,4 +1,4 @@
 sudo add-apt-repository ppa:ubuntugis/ppa
 
-sudo apt-get install postgresql-9.3-postgis-2.1 postgresql-9.3-postgis-2.1-scripts
+sudo apt-get install postgis postgresql-9.3-postgis-2.1 postgresql-9.3-postgis-2.1-scripts
 
