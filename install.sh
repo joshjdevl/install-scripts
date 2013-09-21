a25f8208f99fea21368a5ce3f456a9e340bd1f50
joshj <joshjdevl@gmail.com>
2013-09-21 09:47:44 -0500

    adding aptitude

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
-sudo apt-get -y install git vim python-software-properties wget
+sudo apt-get -y install git vim python-software-properties wget aptitude software-properties-common
 sudo apt-get -y install build-essential autoconf automake gcc g++ make finger libc6-dev pkg-config libreadline-dev zlib1g-dev libxml2-dev
