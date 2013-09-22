e249ac8d1c554b1a7232f4893353e6b18d510c6c
joshj <joshjdevl@gmail.com>
2013-09-22 01:10:40 -0500

    htop

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
-sudo apt-get -y install git vim python-software-properties wget aptitude software-properties-common
+sudo apt-get -y install git vim python-software-properties wget aptitude software-properties-common htop
 sudo apt-get -y libbz2-dev libssl-dev
 sudo apt-get -y install build-essential autoconf automake gcc g++ make finger libc6-dev pkg-config libreadline-dev zlib1g-dev libxml2-dev
