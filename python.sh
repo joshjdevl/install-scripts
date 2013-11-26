812262b9c0535927e58384782b0e868c5d11e18a
joshjdevl <joshjdevl@gmail.com>
2013-11-26 15:13:05 -0600

    python path

--- python.sh
+++ python.sh
@@ -9,7 +9,7 @@ cd
 
 wget -O setuptools-0.6c11-py2.7.egg http://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11-py2.7.egg#md5=fe1f997bc722265116870bc7919059ea
 chmod 755 setuptools-0.6c11-py2.7.egg
-./setuptools-0.6c11-py2.7.egg
+/usr/local/opt/python/v2.7.5/bin/python setuptools-0.6c11-py2.7.egg
 
 wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 /usr/local/opt/python/v2.7.5/bin/python get-pip.py
